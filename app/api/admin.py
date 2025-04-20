ac6eb9ea7ce299fd3b96803801023a20441b61e7
rorex33 <ilaris32@mail.ru>
2025-04-20 23:50:58 +0400

    Зарегестрировал модели

--- app/api/admin.py
+++ app/api/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 from .models import Client, Doctor, Specialization, Appointment
 
-admin.site.register(Post)
+admin.site.register(Client)
+admin.site.register(Doctor)
+admin.site.register(Specialization)
+admin.site.register(Appointment)
